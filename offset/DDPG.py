updated normalization and fixed some other minor issues

offset/DDPG.py
@@ -152,42 +152,49 @@ class DDPG():
             if torch.amin(x) < 0 or torch.amax(x) > 1:
                 print(torch.amin(x), torch.amax(x))
 
+    def norm(self, k: torch.tensor, typ :str):
+        
+        norm = torch.zeros(k.shape)
+        
+        if typ == 'state':
+            norm[...,0] = self.env.T
+            norm[...,1] = self.env.S0
+            norm[...,2] = self.env.X_max
+            
+        if typ == 'policy':
+            norm[...,0] = self.env.nu_max
+            norm[...,1] = 1.0
+            
+        return norm
+
     def normalize(self, k: torch.tensor, typ: str):
         '''
         possible types: "state" and "policy"
         '''
-        norm = torch.ones(k.shape)
-        if typ == 'state':
-            norm[:,0] = norm[:,0] / self.env.T
-            norm[:,1] = norm[:,1] / self.env.S0
-            norm[:,2] = norm[:,2] / self.env.X_max
-        if typ == 'policy':
-            norm[:,0] = norm[:,0] / self.env.nu_max
-        return k/norm
+        norm = self.norm(k, typ)
+            
+        return k / norm
 
     def de_normalize(self, k: torch.tensor, typ: str):
-        norm = torch.ones(k.shape)
-        if typ == 'state':
-            norm[:,0] = norm[:,0] * self.env.T
-            norm[:,1] = norm[:,1] * self.env.S0
-            norm[:,2] = norm[:,2] * self.env.X_max
-        if typ == 'policy':
-            norm[:,0] = norm[:,0] * self.env.nu_max
-        return k*norm
+        
+        norm = self.norm(k, typ)
+            
+        return k * norm
 
     def Update_Q(self, n_iter = 10, mini_batch_size=256, epsilon=0.02):
         
-        # constants
-        nu_rand = torch.exp (epsilon*torch.randn((mini_batch_size,1)))
-        p_rand = (1 + torch.exp (-epsilon*torch.randn((mini_batch_size,1)))) ** -1
-        p_rand = p_rand.squeeze()
-        H = torch.bernoulli(
-            epsilon * torch.ones(mini_batch_size).to(torch.float32))
-
         for i in range(n_iter): 
+            
             t, S, X = self.__grab_mini_batch__(mini_batch_size)
             
             self.Q_main['optimizer'].zero_grad()
+            
+            # used for randomization
+            nu_rand = torch.exp (epsilon*torch.randn((mini_batch_size,)))
+            p_rand = 1.0/(1.0 + torch.exp(-epsilon*torch.randn((mini_batch_size,))))
+            # H = torch.bernoulli(
+            #     epsilon * torch.ones(mini_batch_size).to(torch.float32))
+            H = 1.0* ( torch.rand(mini_batch_size) < epsilon)
             
             # concatenate states
             Y = self.__stack_state__(t, S, X)
@@ -197,27 +204,30 @@ class DDPG():
             a = self.pi['net'](self.normalize(Y, 'state')).detach()
 
             # randomize policy and prob for exploration
-            a[:,0] = a[:,0] * nu_rand.squeeze()
+            a[:,0] = a[:,0] * nu_rand
             a[:,1] = a[:,1] * p_rand * H + \
                 (1 - (1 - a[:,1]) * p_rand) * (1-H)
 
             # get Q
-            Q = self.Q_main['net']( torch.cat((
-                self.normalize(Y, 'state'), 
-                self.normalize(a, 'policy')),axis=1) )
+            Q = self.Q_main['net']( torch.cat(( \
+                                               self.normalize(Y, 'state'), \
+                                               self.normalize(a, 'policy')), \
+                                              axis=1) )
 
             # step in the environment
             Y_p, r = self.env.step(Y, a)
 
             # compute the Q(S', a*)
             # optimal policy at t+1
-            a_p = self.pi['net'](Y_p).detach()
+            a_p = self.pi['net'](self.normalize(Y_p, 'state')).detach()
             
             # compute the target for Q
             target = r.reshape(-1,1) + self.gamma * \
-                self.Q_target['net'](torch.cat((
-                    self.normalize(Y_p, 'state'), 
-                    self.normalize(a_p, 'policy')),axis=1))
+                self.Q_target['net'](torch.cat(( \
+                                                self.normalize(Y_p, 'state'), \
+                                                self.normalize(a_p, 'policy')), \
+                                               axis=1))
+            
             loss = torch.mean((target.detach() - Q)**2)
             
             # compute the gradients
@@ -243,9 +253,10 @@ class DDPG():
 
             a = self.pi['net'](self.normalize(Y, 'state'))
             
-            Q = self.Q_main['net']( torch.cat((
-                self.normalize(Y, 'state'), 
-                self.normalize(a, 'policy')),axis=1) )
+            Q = self.Q_main['net']( torch.cat(( \
+                                               self.normalize(Y, 'state'),
+                                               self.normalize(a, 'policy')),
+                                              axis=1) )
             
             loss = -torch.mean(Q)
                 
@@ -266,7 +277,7 @@ class DDPG():
         self.plot_policy(name=datetime.now().strftime("%H_%M_%S"))
 
         C = 100
-        D = 100
+        D = 200
         
         if len(self.epsilon)==0:
             self.count=0
@@ -289,7 +300,7 @@ class DDPG():
             if np.mod(i+1,n_plot) == 0:
                 
                 self.loss_plots()
-                self.run_strategy(1_000, name= datetime.now().strftime("%H_%M_%S"), N=100)
+                self.run_strategy(1_000, name= datetime.now().strftime("%H_%M_%S"))
                 # self.plot_policy()
                 self.plot_policy(name=datetime.now().strftime("%H_%M_%S"))
                 
@@ -339,10 +350,10 @@ class DDPG():
         if N is None:
             N = self.env.N
         
-        S = torch.zeros((nsims, N+1)).float()
-        X = torch.zeros((nsims, N+1)).float()
-        a = torch.zeros((nsims, 2, N)).float()
-        r = torch.zeros((nsims, N)).float()
+        S = torch.zeros((nsims, N)).float()
+        X = torch.zeros((nsims, N)).float()
+        a = torch.zeros((nsims, 2, N-1)).float()
+        r = torch.zeros((nsims, N-1)).float()
 
 
         S[:,0] = self.env.S0
@@ -350,9 +361,9 @@ class DDPG():
         
         ones = torch.ones(nsims)
 
-        for k in range(N):
+        for k in range(N-1):
             # Y = self.__stack_state__(self.env.dt*t*ones ,S[:,t], X[:,t])
-            Y = self.__stack_state__((k/N) * self.env.T * ones ,S[:,k], X[:,k])
+            Y = self.__stack_state__(self.env.t[k]* ones ,S[:,k], X[:,k])
 
             # normalize : Y (tSX)
             # get policy
@@ -367,16 +378,13 @@ class DDPG():
             
         # print(torch.cat( (torch.amin(a[:,0,:], dim = 0).unsqueeze(-1), torch.amax(a[:,0,:].unsqueeze(-1), dim = 0)) , dim=-1))
         # print(a[:,0,:])
-                
 
         S = S.detach().numpy()
         X  = X.detach().numpy()
         a = a.detach().numpy()
         r = r.detach().numpy()
 
-        t = (1/N) * self.env.T *np.arange(0, N+1)
-
-        plt.figure(figsize=(5,5))
+        plt.figure(figsize=(8,5))
         n_paths = 3
         
         def plot(t, x, plt_i, title ):
@@ -385,7 +393,7 @@ class DDPG():
             # pdb.set_trace()
             qtl= np.quantile(x, [0.05, 0.5, 0.95], axis=0)
 
-            plt.subplot(2, 2, plt_i)
+            plt.subplot(2, 3, plt_i)
             plt.fill_between(t, qtl[0,:], qtl[2,:], alpha=0.5)
             plt.plot(t, qtl[1,:], color='k', linewidth=1)
             plt.plot(t, x[:n_paths, :].T, linewidth=1)
@@ -395,19 +403,23 @@ class DDPG():
             plt.xlabel(r"$t$")
 
 
-        plot(t, (S-S[:,0].reshape(S.shape[0],-1)), 1, r"$S_t-S_0$" )
-        plot(t, X, 2, r"$X_t$")
-        plot(t[:-1], np.cumsum(r, axis=1), 3, r"$r_t$")
+        plot(self.env.t, (S-S[:,0].reshape(S.shape[0],-1)), 1, r"$S_t-S_0$" )
+        plot(self.env.t, X, 2, r"$X_t$")
+        plot(self.env.t[:-1], np.cumsum(r, axis=1), 3, r"$r_t$")
+        plot(self.env.t[:-1], a[:,0,:], 4, r"$\nu_t$")
+        plot(self.env.t[:-1], a[:,1,:], 5, r"$p_t$")
         # plot(t, np.cumsum(r, axis=1), 3, r"$r_t$")
 
-        plt.subplot(2,2, 4)
+        plt.subplot(2, 3, 6)
         plt.hist(np.sum(r,axis=1), bins=51)
 
 
         plt.tight_layout()
         
         plt.savefig("path_"  +self.name + "_" + name + ".pdf", format='pdf', bbox_inches='tight')
-        plt.show()      
+        plt.show()   
+        
+        t = 1.0* self.env.t
         
         return t, S, X, a
 
@@ -425,67 +437,44 @@ class DDPG():
         
         Sm, Xm = torch.meshgrid(S, X,indexing='ij')
 
-        # plot 
-        fig, axs = plt.subplots(2, 2)
-        plt.suptitle("Trade Rate Heatmap over Time", y =1.01, fontsize = 'xx-large')
-
-        t_steps = [0, self.env.T/4, self.env.T/2, (self.env.T - self.env.dt)]
-        
-        for idx, ax in enumerate(axs.flat):
-            t = torch.ones(NS,NX) * self.env.T * t_steps[idx]
-            Y = self.__stack_state__(t, Sm, Xm)
+        def plot(k, lvls, title):
             
-            # normalize : Y (tSX)
-            a = self.pi['net'](self.normalize(Y, 'state').to(torch.float32)).detach().squeeze()
-            cs = ax.contourf(Sm.numpy(), Xm.numpy(), a[:,:,0], 
-                              levels=np.linspace(-self.env.nu_max/2, self.env.nu_max/2, 21),
-                              cmap='RdBu')
-            # print(torch.amin(a[:,:,0]),torch.amax(a[:,:,0]))
-
-            ax.axvline(self.env.S0, linestyle='--', color='k')
-            ax.axhline(self.env.R, linestyle='--', color='k')
-            ax.set_title(r'$t={:.3f}'.format(t_steps[idx]) +'$',fontsize = 'x-large')
-        
-        fig.text(0.5, -0.01, 'OC Price', ha='center',fontsize = 'x-large')
-        fig.text(-0.01, 0.5, 'Inventory', va='center', rotation='vertical',fontsize = 'x-large')
-        # fig.subplots_adjust(right=0.9)   
-
-        cbar_ax = fig.add_axes([1.04, 0.15, 0.05, 0.7])
-        cbar = fig.colorbar(cs, ax=cbar_ax, location='right')
-        # cbar.set_ticks(np.linspace(-self.env.nu_max/2, self.env.nu_max/2, 11))
-        # cbar.set_ticks(np.linspace(-50, 50, 11))
+            # plot 
+            fig, axs = plt.subplots(2, 2)
+            plt.suptitle(title, y =1.01, fontsize = 'xx-large')
             
-        plt.tight_layout()
-        plt.show()
-
-        # plot 
-        fig, axs = plt.subplots(2, 2)
-        plt.suptitle("Generatuib Probability Heatmap over Time", y =1.01, fontsize = 'xx-large')
-
-        t_steps = [0, self.env.T/4, self.env.T/2, (self.env.T - self.env.dt)]
-        
-        for idx, ax in enumerate(axs.flat):
-            t = torch.ones(NS,NX) * self.env.T * t_steps[idx]
-            Y = self.__stack_state__(t, Sm, Xm)
-
-            # normalize : Y (tSX)
-            a = self.pi['net'](self.normalize(Y, 'state').to(torch.float32)).detach().squeeze()
-            cs = ax.contourf(Sm.numpy(), Xm.numpy(), a[:,:,1], 
-                              levels=np.linspace(0, 1, 21),
-                              cmap='RdBu')
-            # print(torch.amin(a[:,:,1]),torch.amax(a[:,:,1]))
-
-            ax.axvline(self.env.S0, linestyle='--', color='k')
-            ax.axhline(self.env.R, linestyle='--', color='k')
-            ax.set_title(r'$t={:.3f}'.format(t_steps[idx]) +'$',fontsize = 'x-large')
-
-        fig.text(0.5, -0.01, 'OC Price', ha='center',fontsize = 'x-large')
-        fig.text(-0.01, 0.5, 'Inventory', va='center', rotation='vertical',fontsize = 'x-large')
-
-        cbar_ax = fig.add_axes([1.04, 0.15, 0.05, 0.7])
-        cbar = fig.colorbar(cs, ax=cbar_ax, location='right')
-               
-  
-        plt.tight_layout()
-        plt.show()
+            t_steps = [0, self.env.T/4, self.env.T/2, (self.env.T - self.env.dt)]
+            
+            for idx, ax in enumerate(axs.flat):
+                t = torch.ones(NS,NX) * t_steps[idx]
+                Y = self.__stack_state__(t, Sm, Xm)
+                
+                # normalize : Y (tSX)
+                a = self.pi['net'](self.normalize(Y, 'state').to(torch.float32)).detach().squeeze()
+                cs = ax.contourf(Sm.numpy(), Xm.numpy(), a[:,:,k], 
+                                  levels=lvls,
+                                  cmap='RdBu')
+                # print(torch.amin(a[:,:,0]),torch.amax(a[:,:,0]))
     
+                ax.axvline(self.env.S0, linestyle='--', color='k')
+                ax.axhline(self.env.R, linestyle='--', color='k')
+                ax.set_title(r'$t={:.3f}'.format(t_steps[idx]) +'$',fontsize = 'x-large')
+            
+            fig.text(0.5, -0.01, 'OC Price', ha='center',fontsize = 'x-large')
+            fig.text(-0.01, 0.5, 'Inventory', va='center', rotation='vertical',fontsize = 'x-large')
+            # fig.subplots_adjust(right=0.9)   
+    
+            cbar_ax = fig.add_axes([1.04, 0.15, 0.05, 0.7])
+            cbar = fig.colorbar(cs, cax=cbar_ax)
+            # cbar.set_ticks(np.linspace(-self.env.nu_max/2, self.env.nu_max/2, 11))
+            # cbar.set_ticks(np.linspace(-50, 50, 11))
+                
+            plt.tight_layout()
+            plt.show()
+        
+        plot(0, 
+             np.linspace(-self.env.nu_max/2, self.env.nu_max/2, 21), 
+             "Trade Rate Heatmap over Time")
+        plot(1, 
+             np.linspace(0,1,21),
+             "Generation Probability Heatmap over Time")    

offset/offset_env.py
@@ -25,19 +25,19 @@ class offset_env():
         self.S0 = S0
         self.R = R 
         self.pen = pen
-        self.X_max = 2 * R
-        self.nu_max = 100
+        self.X_max = 2.0 * R
+        self.nu_max = 100.0
         
         self.N = N
         self.t = np.linspace(0,self.T, self.N)
         self.dt = self.t[1]-self.t[0]  # time steps
-        self.inv_vol = self.sigma * np.sqrt(3 * self.dt)
+        self.inv_vol = self.sigma * np.sqrt(0.5*self.T )
         
     def randomize(self, mini_batch_size=10):
         # experiment with distributions
         # penalty + N(0,1)
         S0 = self.S0 + torch.randn(mini_batch_size) * self.inv_vol 
-        # Unifrom(0,R)
+        # Unifrom(0,x_max)
         X0 = torch.rand(mini_batch_size) * self.X_max
         # randomized time 
         t0 = torch.tensor(np.random.choice(self.t[:-1], size=mini_batch_size, replace=True)).to(torch.float32)